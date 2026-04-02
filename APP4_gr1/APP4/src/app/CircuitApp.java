cdbd259fee064e3d11e72a0b2e29f0a73ac6683d
FungChr <2539272@cegeplimoilou.ca>
2026-04-02 19:04:29 -0400

    Programmation de l'usage des fichiers JSON dans le projet, modifications pour l'interface d'usage. Il reste des petits détails à compléter ou arranger.

--- APP4_gr1/APP4/src/app/CircuitApp.java
+++ APP4_gr1/APP4/src/app/CircuitApp.java
@@ -4,13 +4,14 @@ import java.util.Scanner;
 
 public class CircuitApp {
     public CircuitApp() {
-        String entree = lireString("•\t[R] Tester un autre fichier : Relance le cycle de sélection" + "\n" + "•\t[Q] Quitter : Ferme proprement l'application avec un message de confirmation.");
+        String entree = "";
+        CircuitBuilder circuitBuilder = new CircuitBuilder();
         do {
             entree = lireString("•\t[R] Tester un autre fichier : Relance le cycle de sélection" + "\n" + "•\t[Q] Quitter : Ferme proprement l'application avec un message de confirmation.");
             if (!validerEntree(entree)){
                 System.out.println("Entrée invalide, entrez «R» pour relancer le cycle de sélection ou «Q» pour quitter l'application.");
             } else {
-                System.out.println("Résistance équivalente calculée : " + "Ω");
+                System.out.println("Résistance équivalente calculée : " + circuitBuilder.construireCircuit("complexe_industriel_zone_nord.json") + "Ω");
             }
         } while (entree.equalsIgnoreCase("Q"));
     }
--- APP4_gr1/APP4/src/app/CircuitBuilder.java
+++ APP4_gr1/APP4/src/app/CircuitBuilder.java
@@ -1,14 +1,66 @@
 package app;
 
-import electronique.Composant;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import electronique.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class CircuitBuilder {
 
-    public Composant construireCircuit(String circuit){
+    private final static char fSep = File.separatorChar;
+    private static final String pathIn = System.getProperty("user.dir") + fSep + "APP4_gr1" + fSep + "APP4" + fSep + "src" + fSep + "donnees" + fSep + "fichiers_json" + fSep;
+
+    public CircuitBuilder() {
+    }
+
+    public Composant construireCircuit(String cheminFichier){
+
+        ObjectMapper mapper = new ObjectMapper();
+        Composant circuit = null;
+
+        cheminFichier = pathIn + cheminFichier;
+        try {
+            JsonNode donneesCircuit = mapper.readTree(new File(cheminFichier));
+
+                JsonNode circuitLecture = donneesCircuit.get("circuit");
+
+                return lireComposant(circuitLecture);
+
+        } catch (IOException e) {
+            System.out.println("Erreur de lecture : " + e.getMessage());
+        }
+
         return null;
     }
 
-    private Composant lireComposant(){
-        return null;
+    private Composant lireComposant(JsonNode node){
+        String type = node.get("type").asText();
+
+        if("resistance".equals(type)){
+            return new Resistance(node.get("valeur").asDouble());
+        }
+
+        else if ("serie".equals(type)){
+            ArrayList<Composant> composants = new ArrayList<>();
+
+            for (JsonNode compoosantNode : node.get("composants")){
+                composants.add(lireComposant(compoosantNode));
+            }
+            return new CircuitSerie(composants);
+        }
+
+        else if ("parallele".equals(type)){
+            ArrayList<Composant> composants = new ArrayList<>();
+
+            for (JsonNode compoosantNode : node.get("composants")){
+                composants.add(lireComposant(compoosantNode));
+            }
+            return new CircuitParallele(composants);
+        }
+
+        throw new IllegalArgumentException("Type de composant incconu : " + type);
     }
 }
